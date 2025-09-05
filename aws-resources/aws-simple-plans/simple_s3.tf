e8279f36c19dcd94a89d34ca44d7fe8c91eb06bf
Heshercat <alfiiaqa@gmail.com>
2025-09-05 05:08:05 +0300

    Add OPA for infracost

--- aws-resources/aws-simple-plans/simple_s3.tf
+++ aws-resources/aws-simple-plans/simple_s3.tf
@@ -14,7 +14,19 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "alfiia-s3-simple-${replace(time_static.now.unix, "/[^0-9]/", "")}"
+  count = 10
+  bucket = "alfiia-s3-${count.index}-simple-${replace(time_static.now.unix, "/[^0-9]/", "")}"
+  force_destroy = true
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Test"
+  }
+}
+
+resource "aws_s3_bucket" "example2" {
+  count = 1
+  bucket = "alfiia-s3-2-${count.index}-simple-${replace(time_static.now.unix, "/[^0-9]/", "")}"
   force_destroy = true
 
   tags = {
